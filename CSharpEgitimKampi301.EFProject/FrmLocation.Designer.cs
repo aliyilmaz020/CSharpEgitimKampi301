541fcc1f909ed23cc524795631da5322704a7038
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2024-11-20 00:52:22 +0300

    .

--- CSharpEgitimKampi301.EFProject/FrmLocation.Designer.cs
+++ CSharpEgitimKampi301.EFProject/FrmLocation.Designer.cs
@@ -135,7 +135,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(831, 460);
+            this.dataGridView1.Size = new System.Drawing.Size(956, 460);
             this.dataGridView1.TabIndex = 15;
             // 
             // btnList
