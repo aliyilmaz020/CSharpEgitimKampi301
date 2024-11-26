5034da1e77828c6b829a1e2977bf83e9cb3e0e93
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2024-11-26 19:40:14 +0300

    Created Generic Repository Class

---
+++ CSharpEgitimKampi301.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
