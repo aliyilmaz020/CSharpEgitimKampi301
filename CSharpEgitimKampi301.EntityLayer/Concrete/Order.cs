5014f1121cdb5b96f4419c2985538373086fa554
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2024-12-04 23:52:19 +0300

    migration

--- CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -10,7 +10,7 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
     {
         public int OrderId { get; set; }
         public int ProductId  { get; set; }
-        public Product Product { get; set; }
+        public virtual Product Product { get; set; }
         public int Quantity { get; set; }
         public decimal TotalPrice { get; set; }
         public int CustomerId { get; set; }
