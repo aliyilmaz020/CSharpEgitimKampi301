ba804177e962778e4b57ebf031dfe5a13d6dca4d
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2024-12-04 15:35:30 +0300

    Business Layer Manager Classes created and implemented

---
+++ CSharpEgitimKampi301.BusinessLayer/Concrete/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.BusinessLayer.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        public void TDelete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Customer> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public Customer TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void TInsert(Customer entity)
+        {
+            
+        }
+        public void TUpdate(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
